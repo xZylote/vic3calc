d7af7b5f320aed90dd442f641f3d9c15d866022d
Luca Bosch <luca.bosch@uni-konstanz.de>
2022-11-08 03:31:13 +0100

    Enable adding multiple factories

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-let $ = (x) => document.getElementById(x)
-let _ = (x) => document.createElement(x)
-let multiplier = 1
+$ = function (x) {
+    let y = document.querySelectorAll(x)
+    if (y.length === 0) return null
+    if (y.length === 1) return y[0]
+    return y
+}
 
-const data = { buildings: {}, goods: {}, production_methods: {}, production_method_groups: {} }
-let dropdown_created = false
+_ = (x) => document.createElement(x)
+
+//------------------------------------//
+
+let data = { buildings: {}, goods: {}, production_methods: {}, production_method_groups: {} }
+
 for (let type in files) {
     for (let path of files[type]) {
         jomini.Jomini.initialize().then((parser) => {
-            const out = parser.parseText(readFile("./" + type + "/" + path));
+            let out = parser.parseText(readFile('./' + type + '/' + path))
             for (let prop in out) {
                 data[type][prop] = out[prop]
             }
-        });
+        })
     }
 }
 
 function readFile(path) {
     let data
     rawFile = new XMLHttpRequest()
-    rawFile.open("GET", path, false)
+    rawFile.open('GET', path, false)
     rawFile.onreadystatechange = () => {
         if (rawFile.readyState === 4) {
             if (rawFile.status === 200 || rawFile.status == 0) {
@@ -30,32 +37,26 @@ function readFile(path) {
     return data
 }
 
-
-
-function bar(selection) {
-    $("info").innerHTML = ""
-    if (selection !== "Select Building") createTable(selection)
-    for (input of document.getElementsByTagName("input")) {
-        input.addEventListener("click", () => { calculateSum($("good").options[$("good").selectedIndex].value) })
-    }
-}
-
-function createDropdown() {
-    if (!dropdown_created) {
-        for (var entry in data.buildings) {
-            if (!(data.buildings[entry].buildable == false) && !(data.buildings[entry].expandable == false)) { // careful with that javascript
-                var option = document.createElement("option");
-                option.value = entry
-                option.innerText = entry.substring(9).split("_").map(word => { return word.charAt(0).toUpperCase() + word.slice(1) }).join(' ')
-                $("good").appendChild(option)
-            }
+function bar() {
+    let selection = []
+    let index = 0
+    for (let div of document.querySelectorAll('.input-group')) {
+        let a = div.querySelector('select')
+        if (a.value !== 'null') {
+            selection.push({ name: a.value, amount: div.querySelector('input').value, index: index })
         }
-        dropdown_created = true
+        index++
+    }
+
+    $('#info').innerHTML = ''
+
+    for (let factory of selection) {
+        createTable(factory)
     }
 }
 
 function joinTables() {
-    $("mask").remove()
+    $('#mask').remove()
     for (let production_method_group in data.production_method_groups) {
         for (let production_method1 of data.production_method_groups[production_method_group].production_methods) {
             for (let production_method2 in data.production_methods) {
@@ -81,59 +82,57 @@ function joinTables() {
 }
 
 function createTable(selection) {
-    var table = _("table")
-    var table_production_method_groups = _("table")
-    var tr_header = _("tr")
-    var tr_selected = _("tr")
-    var tr_production_method_groups = _("tr")
-    var th_name = _("th"),
-        th_multiplier = _("th"),
-        th_technologies = _("th"),
-        th_production_method_groups = _("th")
-    var td_name = _("td"),
-        td_multiplier = _("th"),
-        td_technologies = _("td"),
-        td_production_method_groups = _("td")
-    var building_thumbnail = _("img")
-    var mult_input = _("input")
-    for (let production_method_group in data.buildings[selection].production_method_groups) {
-        var th_production_method_group = _("th")
-        var td_production_method_group = _("td")
-        for (let production_method in data.buildings[selection].production_method_groups[production_method_group].production_methods) {
-            var tr_production_methods = _("tr")
-            var radio_wrapper = _("label")
-            var production_method_radio = _("input")
-            var production_method_thumbnail = _("img")
+    let table_container = _('div')
+    let table = _('table')
+    table.id = 'table_' + selection.index
+    let table_production_method_groups = _('table')
+    let tr_header = _('tr')
+    let tr_selected = _('tr')
+    let tr_production_method_groups = _('tr')
+    let th_name = _('th'),
+        th_multiplier = _('th'),
+        th_technologies = _('th'),
+        th_production_method_groups = _('th')
+    let td_name = _('td'),
+        td_multiplier = _('th'),
+        td_technologies = _('td'),
+        td_production_method_groups = _('td')
+    let building_thumbnail = _('img')
+    let mult_input = _('p')
 
-            production_method_radio.checked = true // Dirty, but will select all the last ones efficiently#
-            production_method_radio.type = "radio"
-            production_method_radio.name = data.buildings[selection].production_method_groups[production_method_group].name
-            production_method_radio.value = data.buildings[selection].production_method_groups[production_method_group].production_methods[production_method].name
-            production_method_thumbnail.title = data.buildings[selection].production_method_groups[production_method_group].production_methods[production_method].name
-            production_method_thumbnail.src = data.buildings[selection].production_method_groups[production_method_group].production_methods[production_method].texture.replace("dds", "jpg")
+    for (let production_method_group in data.buildings[selection.name].production_method_groups) {
+        let th_production_method_group = _('th')
+        let td_production_method_group = _('td')
+        for (let production_method in data.buildings[selection.name].production_method_groups[production_method_group].production_methods) {
+            let tr_production_methods = _('tr')
+            let radio_wrapper = _('label')
+            let production_method_radio = _('input')
+            let production_method_thumbnail = _('img')
+            production_method_radio.onclick = () => calculateSum(selection, table_container)
+            production_method_radio.checked = true // Dirty, but will select all the last ones efficiently
+            production_method_radio.type = 'radio'
+            production_method_radio.name = data.buildings[selection.name].production_method_groups[production_method_group].name
+            production_method_radio.value = data.buildings[selection.name].production_method_groups[production_method_group].production_methods[production_method].name
+            production_method_thumbnail.title = data.buildings[selection.name].production_method_groups[production_method_group].production_methods[production_method].name
+            production_method_thumbnail.src = data.buildings[selection.name].production_method_groups[production_method_group].production_methods[production_method].texture.replace('dds', 'jpg')
 
             radio_wrapper.append(production_method_radio, production_method_thumbnail)
             tr_production_methods.append(radio_wrapper)
             td_production_method_group.append(tr_production_methods)
         }
-        th_production_method_group.innerText = data.buildings[selection].production_method_groups[production_method_group].name
-
+        th_production_method_group.innerText = data.buildings[selection.name].production_method_groups[production_method_group].name
         tr_production_method_groups.append(td_production_method_group)
         table_production_method_groups.append(th_production_method_group, tr_production_method_groups)
     }
 
-
-    th_name.innerText = "Building"
-    th_technologies.innerText = "Required Technologies"
-    th_multiplier.innerText = "#"
-    th_production_method_groups.innerText = "Production Methods"
-    building_thumbnail.title = selection
-    building_thumbnail.src = data.buildings[selection].texture.replace("dds", "jpg")
-    mult_input.type = "number"
-    mult_input.id = "mult_input"
-    mult_input.value = 1
-    mult_input.addEventListener('change', () => { multiplier = mult_input.value })
-    td_technologies.innerText = data.buildings[selection].unlocking_technologies
+    th_name.innerText = 'Building'
+    th_technologies.innerText = 'Required Technologies'
+    th_multiplier.innerText = '#'
+    th_production_method_groups.innerText = 'Production Methods'
+    building_thumbnail.title = selection.name
+    building_thumbnail.src = data.buildings[selection.name].texture.replace('dds', 'jpg')
+    mult_input.innerText = selection.amount
+    td_technologies.innerText = data.buildings[selection.name].unlocking_technologies
 
     td_multiplier.append(mult_input)
     td_name.append(building_thumbnail)
@@ -142,37 +141,43 @@ function createTable(selection) {
     tr_selected.append(td_multiplier, td_name, td_technologies, td_production_method_groups)
     table.append(tr_header)
     table.append(tr_selected)
-    $("info").append(table)
+    table_container.append(table)
+    $('#info').append(table_container)
 
-    calculateSum(selection)
+    calculateSum(selection, table_container)
 }
 
-function calculateSum(selection) {
+function calculateSum(selection, table_container) {
+
     let i = 0
-    var indices = [],
+    let indices = [],
         balance = [],
         balanceobj = { input: {}, output: {}, employment: {} }
-    var radioButtons = document.querySelectorAll("input[type='radio']");
-    for (var radioButton of radioButtons) {
+    let radioButtons = $('#table_' + selection.index).querySelectorAll('input[type="radio"]')
+
+    for (let radioButton of radioButtons) {
         if (radioButton.checked) {
             indices.push(Array.prototype.indexOf.call(radioButton.parentElement.parentElement.parentElement.childNodes, radioButton.parentElement.parentElement))
         }
     }
 
-    for (let checked_radio of document.querySelectorAll('input[type="radio"]:checked')) {
-        if (data.buildings[selection].production_method_groups[i].production_methods[indices[i]].building_modifiers) {
-            balance.push(data.buildings[selection].production_method_groups[i].production_methods[indices[i]].building_modifiers.unscaled)
-            balance.push(data.buildings[selection].production_method_groups[i].production_methods[indices[i]].building_modifiers.workforce_scaled)
-            balance.push(data.buildings[selection].production_method_groups[i].production_methods[indices[i]].building_modifiers.level_scaled)
+    for (let e of $('#table_' + selection.index).querySelectorAll('input[type="radio"]:checked')) {
+        if (data.buildings[selection.name].production_method_groups[i].production_methods[indices[i]].building_modifiers) {
+            balance.push(data.buildings[selection.name].production_method_groups[i].production_methods[indices[i]].building_modifiers.unscaled)
+            balance.push(data.buildings[selection.name].production_method_groups[i].production_methods[indices[i]].building_modifiers.workforce_scaled)
+            balance.push(data.buildings[selection.name].production_method_groups[i].production_methods[indices[i]].building_modifiers.level_scaled)
         }
         i++
     }
 
-    balance = balance.filter(Boolean); // Remove falsy values
-    for (var element of balance) {
-        for (var subelement in element) {
-            var temp = subelement.split("_")
-            if (["input", "output", "employment"].includes(temp[1])) {
+    balance = balance.filter(Boolean)
+    for (let element of balance) {
+        for (let subelement in element) {
+            let temp = subelement.split('_')
+            if (temp.length === 5) {
+                temp[2] = temp[2] + '_' + temp[3]
+            }
+            if (['input', 'output', 'employment'].includes(temp[1])) {
                 if (balanceobj[temp[1]][temp[2]]) {
                     balanceobj[temp[1]][temp[2]] += element[subelement]
                 } else {
@@ -182,48 +187,97 @@ function calculateSum(selection) {
 
         }
     }
-
-    createSumTable(balanceobj)
+    createSumTable(balanceobj, selection, table_container)
 }
 
-function createSumTable(balance) {
-    if ($("x92")) $("x92").remove()
-    var sum = _("table")
-    sum.id = "x92"
-    var resources_header = _("tr")
-    var resources = _("tr")
-    var th_output = _("th"),
-        th_input = _("th"),
-        th_employment = _("th")
-    var td_output = _("td"),
-        td_input = _("td"),
-        td_employment = _("td")
+function createSumTable(balance, selection, table_container) {
+    console.log(balance)
+    let exists = $('#table_' + selection.index).parentElement.querySelector('.sum_table')
+    if (exists) {
+        exists.remove()
+    }
+    let sum = _('table')
+    sum.classList.add('sum_table')
+    let resources_header = _('tr')
+    let resources = _('tr')
+    let th_output = _('th'),
+        th_input = _('th'),
+        th_employment = _('th')
+    let td_output = _('td'),
+        td_input = _('td'),
+        td_employment = _('td')
 
-    th_output.innerText = "Output"
-    th_input.innerText = "Input"
-    th_employment.innerText = "Employment"
+    th_output.innerText = 'Output'
+    th_input.innerText = 'Input'
+    th_employment.innerText = 'Employment'
 
-    for (var item in balance.output) {
-        var tr = _("tr")
-        tr.innerText += item + ": " + balance.output[item] * multiplier
+    for (let item in balance.output) {
+        let tr = _('tr')
+        tr.innerText += item + ': ' + balance.output[item] * selection.amount
         td_output.append(tr)
     }
-    for (var item in balance.input) {
-        var tr = _("tr")
-        tr.innerText += item + ": " + balance.input[item] * multiplier
+    for (let item in balance.input) {
+        let tr = _('tr')
+        tr.innerText += item + ': ' + balance.input[item] * selection.amount
+        td_output.append(tr)
         td_input.append(tr)
     }
-    for (var item in balance.employment) {
-        var tr = _("tr")
-        tr.innerText += item + ": " + balance.employment[item] * multiplier
+    for (let item in balance.employment) {
+        let tr = _('tr')
+        tr.innerText += item + ': ' + balance.employment[item] * selection.amount
+        td_output.append(tr)
         td_employment.append(tr)
     }
-
-
 
     resources_header.append(th_output, th_input, th_employment)
     resources.append(td_output, td_input, td_employment)
     sum.append(resources_header, resources)
-
-    $("info").append(sum)
+    //let details = _('details')
+    //details.append(sum)
+    //table_container.append(details)
+    table_container.append(sum)
 }
+
+function addFactory() {
+    let container = $('#header'),
+        div = _('div'),
+        select = _('select'),
+        input = _('input'),
+        option = _('option'),
+        xbutton = _('button')
+
+    select.classList.add('form-select')
+    input.type = 'number'
+    input.value = 1
+    input.classList.add('form-control')
+    option.setAttribute('value', null)
+    option.innerHTML = '- Select Building -'
+    select.onchange = () => bar()
+    input.onchange = () => bar()
+    xbutton.classList.add('btn', 'btn-danger')
+    div.classList.add('input-group')
+    xbutton.onclick = function () {
+        this.parentElement.remove()
+        bar()
+    }
+
+    for (let entry in data.buildings) {
+        if (!(data.buildings[entry].buildable == false) && !(data.buildings[entry].expandable == false)) {
+            let option = _('option')
+            option.value = entry
+            option.innerText = entry.substring(9).split('_').map(word => { return word.charAt(0).toUpperCase() + word.slice(1) }).join(' ')
+            select.append(option)
+        }
+    }
+
+    xbutton.append(document.createTextNode('Remove'))
+    div.append(input)
+    select.append(option)
+    div.append(select)
+    div.append(xbutton)
+    container.append(div)
+}
+
+//value-lookup
+//save preferred production methods
+//default era production methods
